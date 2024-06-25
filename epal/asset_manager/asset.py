4b716442368722e4e0f999710ba3d94825ff8a87
TheHerowither <lucland278@gmail.com>
2024-06-25 16:42:38 +0200

    Fix type annotation in epal.asset_manager.asset

--- epal/asset_manager/asset.py
+++ epal/asset_manager/asset.py
@@ -41,10 +41,10 @@ class Asset:
         self.__asset__ = pygame.transform.scale(self.get(), new_size)
     
     @classmethod
-    def __load_from_binary__(cls, name : str, data : bytes, asset_info = dict):
+    def __load_from_binary__(cls, name : str, data : bytes, asset_info : dict):
         ret =  Asset(name)
         if asset_info["type"] == AssetType.Image:
-            ret.__asset__ = pygame.image.frombuffer(data, asset_info["size"], "RGBA")
+            ret.__asset__ = pygame.image.frombuffer(data, asset_info["type"], "RGBA")
         if asset_info["type"] == AssetType.Audio:
             ret.__asset__ = pygame.mixer.Sound(buffer = data)
         ret.loaded = True
