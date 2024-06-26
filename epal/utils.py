7d267f148e9261d945603e84d5d38b48871dc913
TheHerowither <lucland278@gmail.com>
2024-06-26 14:14:05 +0200

    Add typehint to epal.Vector2 operators

--- epal/utils.py
+++ epal/utils.py
@@ -1,4 +1,4 @@
-
+from typing import Self
 
 class Vector2:
     def __init__(self, x : int, y : int):
@@ -8,11 +8,11 @@ class Vector2:
     def as_tuple(self) -> tuple[int, int]:
         return (self.x, self.y)
     
-    def __add__(self, other):
+    def __add__(self, other) -> Self:
         return Vector2(self.x + other.x, self.y + other.y)
-    def __sub__(self, other):
+    def __sub__(self, other) -> Self:
         return Vector2(self.x - other.x, self.y - other.y)
-    def __mul__(self, other):
+    def __mul__(self, other) -> Self:
         return Vector2(self.x * other, self.y * other)
-    def __div__(self, other):
+    def __div__(self, other) -> Self:
         return Vector2(self.x / other, self.y / other)
